1ab235eca08a5e3b3641d4c205f37d712273ecd5
Yogender <yashmittal92002@gmail.com>
2025-08-27 13:12:08 +0530

    updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,14 +6,22 @@ const Form = () => {
     
     // <Card>
    <form id="info-form">
+    <div>
     <label htmlFor='full_name'>Full Name</label>
     <input type="text" id="full_name"/>
+    </div>
+    <div>
     <label htmlFor='email'>Email</label>
     <input type="email" id="email"/>
+    </div>
+    <div>
     <label htmlFor='password'>Password</label>
     <input type="password" id="password"/>
+    </div>
+    <div>
     <label htmlFor='password_confirmation'>Confirm Password</label>
     <input type="password" id="password_confirmation"/>
+    </div>
     <button type="submit">Submit</button>
    </form>
 //    </Card>
--- src/components/FormRef.jsx
+++ src/components/FormRef.jsx
@@ -10,14 +10,22 @@ const FormRef = () => {
     //label
     // <Card>
     <form id="info-form">
+        <div>
     <label htmlFor='full_name'>Full Name</label>
     <input type="text" id="full_name" ref={nameref}/>
+    </div>
+    <div>
     <label htmlFor='email'>Email</label>
     <input type="email" id="email" ref={mailref}/>
+    </div>
+    <div>
     <label htmlFor='password'>Password</label>
     <input type="password" id="password" ref={passwordref}/>
+    </div>
+    <div>
     <label htmlFor='password_confirmation'>Confirm Password</label>
     <input type="password" id="password_confirmation" ref={confirmref}/>
+    </div>
     <button type="submit" onClick={(e)=>{
         e.preventDefault();
         // console.log(nameref.current.value);
